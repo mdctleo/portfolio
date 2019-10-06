80b92ddc5707b584a36ed2f613e6c229a319b736
mdctleo <mdctleo@hotmail.com>
2019-10-06 15:53:37 -0700

    initial commit

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,35 +1,35 @@
 var plugins = [{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-remark-images/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-remark-images/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-personal-portfolio","short_name":"mohan's portfolio","start_url":"/","background_color":"#616CAA","theme_color":"#616CAA","display":"minimal-ui","icon":"src/images/code_logo.png"},
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      options: {"plugins":[],"name":"gatsby-personal-portfolio","short_name":"mohan's portfolio","start_url":"/","background_color":"#616CAA","theme_color":"#616CAA","display":"minimal-ui","icon":"src/favicon.png"},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
-      options: {"plugins":[{"resolve":"G:/Gatsby/portfolio/node_modules/gatsby-remark-images","id":"3626c0e0-58d4-5046-b0e5-cb7731a052f2","name":"gatsby-remark-images","version":"3.1.20","pluginOptions":{"plugins":[]},"nodeAPIs":[],"browserAPIs":["onRouteUpdate"],"ssrAPIs":["onRenderBody"]},{"resolve":"G:/Gatsby/portfolio/node_modules/gatsby-remark-autolink-headers","id":"df380aaa-460e-51a2-8830-e6712dc1f436","name":"gatsby-remark-autolink-headers","version":"2.1.9","pluginOptions":{"plugins":[]},"nodeAPIs":[],"browserAPIs":["onInitialClientRender","shouldUpdateScroll"],"ssrAPIs":["onRenderBody"]}],"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-external-links","options":{"target":"_blank","rel":"nofollow noopener noreferrer"}},{"resolve":"gatsby-remark-images","options":{"maxWidth":830,"quality":90,"withWebp":true,"linkImagesToOriginal":false}},{"resolve":"gatsby-remark-autolink-headers","options":{"maintainCase":false}}]},
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
+      options: {"plugins":[{"resolve":"/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-remark-images","id":"3626c0e0-58d4-5046-b0e5-cb7731a052f2","name":"gatsby-remark-images","version":"3.1.20","pluginOptions":{"plugins":[]},"nodeAPIs":[],"browserAPIs":["onRouteUpdate"],"ssrAPIs":["onRenderBody"]},{"resolve":"/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-remark-autolink-headers","id":"df380aaa-460e-51a2-8830-e6712dc1f436","name":"gatsby-remark-autolink-headers","version":"2.1.9","pluginOptions":{"plugins":[]},"nodeAPIs":[],"browserAPIs":["onInitialClientRender","shouldUpdateScroll"],"ssrAPIs":["onRenderBody"]}],"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-external-links","options":{"target":"_blank","rel":"nofollow noopener noreferrer"}},{"resolve":"gatsby-remark-images","options":{"maxWidth":830,"quality":90,"withWebp":true,"linkImagesToOriginal":false}},{"resolve":"gatsby-remark-autolink-headers","options":{"maintainCase":false}}]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-remark-images/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-remark-images/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-remark-autolink-headers/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-remark-autolink-headers/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"Minimal Blog - Gatsby Theme","short_name":"MinimalBlog","description":"Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.","background_color":"#FFF","theme_color":"#616CAA","display":"standalone","icon":"src/favicon.png"},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('G:/Gatsby/portfolio/node_modules/gatsby-tech-blog-theme/gatsby-ssr'),
+      plugin: require('/Users/leolin/Desktop/project/portfolio-gatsby/gatsby_starter_portfolio/node_modules/gatsby-tech-blog-theme/gatsby-ssr'),
       options: {"plugins":[],"basePath":"/blog","contentPath":"content/blog"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
